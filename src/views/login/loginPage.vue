be4d3c71503c34b11430eb9ade9674de392a4d7b
baizhiming <bai@gmail.com>
2024-10-23 18:07:48 +0800

    axios实例的配置

--- src/views/login/loginPage.vue
+++ src/views/login/loginPage.vue
@@ -1,14 +1,32 @@
 <script setup>
 import { ref } from 'vue'
+import { User, Lock } from '@element-plus/icons-vue'
 
 const isRegister = ref(false)
 </script>
 <template>
-  <el-row class="login-page">
-    <el-col :span="12" class="bg"></el-col>
-    <el-col :span="6" :offset="3" class="form">
-      <!-- 注册 -->
-      <el-form size="large" autocomplete="off" v-if="isRegister">
+  <el-row>
+    <el-col class="login-page" :span="12">
+      <el-col :span="7" :offset="7" class="left">
+        <section class="image">
+          <img src="@/assets/zqzxlogo.png" alt="" />
+        </section>
+        <!--        文字部分-->
+        <section>
+          <h3 class="title">欢迎使用 智企知心</h3>
+          <ul class="ul-li">
+            <li>智能对话</li>
+            <li>知心图文</li>
+            <li>提取文字</li>
+            <li>工作报告</li>
+            <li>特色模型</li>
+          </ul>
+        </section>
+      </el-col>
+    </el-col>
+    <el-col :span="6" :offset="3">
+      <!--      注册-->
+      <el-form size="large" autocomplete="off" v-if="isRegister" class="form">
         <el-form-item>
           <h1>注册</h1>
         </el-form-item>
@@ -16,32 +34,24 @@ const isRegister = ref(false)
           <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-input
-            :prefix-icon="Lock"
-            type="password"
-            placeholder="请输入密码"
-          ></el-input>
+          <el-input :prefix-icon="Lock" placeholder="请输入密码"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-input
-            :prefix-icon="Lock"
-            type="password"
-            placeholder="请输入再次密码"
-          ></el-input>
+          <el-input :prefix-icon="Lock" placeholder="请再次输入密码"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button class="button" type="primary" auto-insert-space>
-            注册
+          <el-button type="primary" auto-insert-space class="large1"
+            >注册
           </el-button>
         </el-form-item>
-        <el-form-item class="flex">
-          <el-link type="info" :underline="false" @click="isRegister = false">
-            ← 返回
+        <el-form-item>
+          <el-link type="info" :underline="false" @click="isRegister = false"
+            >← 返回
           </el-link>
         </el-form-item>
       </el-form>
-      <!-- 登录 -->
-      <el-form size="large" autocomplete="off" v-else>
+      <!--      登录-->
+      <el-form size="large" autocomplete="off" v-else class="form">
         <el-form-item>
           <h1>登录</h1>
         </el-form-item>
@@ -49,59 +59,79 @@ const isRegister = ref(false)
           <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-input
-            name="password"
-            :prefix-icon="Lock"
-            type="password"
-            placeholder="请输入密码"
-          ></el-input>
+          <el-input :prefix-icon="Lock" placeholder="请输入密码"></el-input>
         </el-form-item>
-        <el-form-item class="flex">
-          <div class="flex">
-            <el-checkbox>记住我</el-checkbox>
-            <el-link type="primary" :underline="false">忘记密码？</el-link>
-          </div>
+        <section class="flex">
+          <el-checkbox>记住密码</el-checkbox>
+          <el-link type="primary" :underline="false">忘记密码？</el-link>
+        </section>
+        <el-form-item></el-form-item>
+        <el-form-item>
+          <el-button type="primary" auto-insert-space class="large1"
+            >登录
+          </el-button>
         </el-form-item>
         <el-form-item>
-          <el-button class="button" type="primary" auto-insert-space
-            >登录</el-button
-          >
-        </el-form-item>
-        <el-form-item class="flex">
-          <el-link type="info" :underline="false" @click="isRegister = true">
-            注册 →
+          <el-link type="info" :underline="false" @click="isRegister = true"
+            >注册 →
           </el-link>
         </el-form-item>
       </el-form>
     </el-col>
   </el-row>
 </template>
-<style lang="scss" scoped>
+<style scoped>
+body {
+  font-size: 10px;
+}
+
+/* 左边 */
 .login-page {
   height: 100vh;
-  background-color: #fff;
-  .bg {
-    background:
-      url('@/assets/login-page-bg.jpg') no-repeat;
-      background-size: cover;
-    border-radius: 0 20px 20px 0;
-  }
-  .form {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    user-select: none;
-    .title {
-      margin: 0 auto;
-    }
-    .button {
-      width: 100%;
-    }
-    .flex {
-      width: 100%;
-      display: flex;
-      justify-content: space-between;
-    }
-  }
+  background-image: url('@/assets/login-page-bg.jpg');
+  background-size: cover;
+}
+
+.left {
+  margin-top: 19vh;
+}
+
+.image {
+  width: 7rem;
+  height: 7rem;
+}
+
+.image img {
+  width: 100%;
+}
+
+.title{
+  font-size: 1.3rem;
+  margin-bottom: 0.5rem;
+}
+
+.ul-li{
+  list-style: none;
+  padding: 0;
+  margin: 0 0 0 2rem;
+}
+ul li{
+  font-size: 1rem;
+  margin-bottom: 0.3rem;
+}
+
+/* 右边 表单 */
+.form {
+  padding-top: 20vh;
+}
+
+.flex {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.form .large1 {
+  width: 100vh;
 }
 </style>
